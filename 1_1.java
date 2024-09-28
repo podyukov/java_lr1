a1c349c30a1cd51d4c5a7e9f00b86f521914af34
podyukov <98814291+podyukov@users.noreply.github.com>
2024-09-28 21:17:31 +0500

    1_1.java

--- 1_1.java
+++ 1_1.java
@@ -1,5 +1,6 @@
 package ru.podyukov.java;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -7,8 +8,12 @@ public class Main {
         Main m = new Main();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите дробное число: ");
-        float n = (float) m.fraction(scanner.nextDouble()); // приводим ко float, чтобы не было 0,120000001
-        System.out.println("Результат: " + n);
+        try {
+            float n = (float) m.fraction(scanner.nextDouble()); // приводим ко float, чтобы не было 0,120000001
+            System.out.println("Результат: " + n);
+        } catch (InputMismatchException e) {
+            System.out.println("Введены некорректные данные! Выход из программы!");
+        }
     }
     public double fraction (double x) {
         return x % 1; // берем остаток от деления на 1

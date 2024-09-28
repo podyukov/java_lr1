16c45fb87f59f3bdd35e182bfdf84848fc17b038
podyukov <98814291+podyukov@users.noreply.github.com>
2024-09-28 21:59:20 +0500

    1_5.java

--- 1_5.java
+++ 1_5.java
@@ -1,14 +1,20 @@
 package ru.podyukov.java;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Main m = new Main();
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите число: ");
-        boolean b = m.is2Digits(scanner.nextInt());
-        System.out.println(b);
+        try {
+            System.out.print("Введите число: ");
+            boolean b = m.is2Digits(scanner.nextInt());
+            System.out.println(b);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Введено неверное значение! Выход!");
+        }
     }
     public boolean is2Digits (int x) {
         if ((x / 10) >= 1 && (x / 10) <= 9) return true;
